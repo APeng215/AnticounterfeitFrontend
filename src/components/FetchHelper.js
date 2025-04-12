59bd965c747c243ddf991fd1c3d129ce13ec4f22
A_Peng215 <976310604@qq.com>
2025-04-12 14:42:33 +0800

    Add adding goods function

--- src/components/FetchHelper.js
+++ src/components/FetchHelper.js
@@ -14,7 +14,32 @@ export default class FetchHelper {
       throw error;
     }
   }
+
+  static async post(relevantPath, data) {
+    try {
+      relevantPath = this.#formatRelevantPath(relevantPath);
+      const url = this.backEndIP + relevantPath;
+
+      const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
+
+      return res;
+    } catch (error) {
+      console.error('Fetch error:', error);
+      throw error;
+    }
+  }
+
   static #formatRelevantPath(relevantPath) {
     return relevantPath.startsWith('/') ? relevantPath : `/${relevantPath}`;
   }
+
+
 }
--- src/pages/goods-management.vue
+++ src/pages/goods-management.vue
@@ -4,9 +4,12 @@ import FetchHelper from "@/components/FetchHelper.js";
 export default {
   data() {
     return {
-      goods: [
-
-      ]
+      goods: [],
+      dialog: false,
+      addGoodsDialog: {
+        name: null,
+        description: null,
+      }
     }
   },
   mounted() {
@@ -17,6 +20,15 @@ export default {
       FetchHelper.get("/goods").then((data) => {
         this.goods = data;
       });
+    },
+    goodsNameRequired (goodsName) {
+      return !!goodsName || '商品名称不能为空'
+    },
+    addGoodsButtonClicked() {
+      FetchHelper.post("/goods", this.addGoodsDialog)
+        .then(() => {
+        })
+      this.dialog = false;
     }
   }
 }
@@ -31,14 +43,61 @@ export default {
             商品
           </v-toolbar-title>
 
-          <v-btn
-            class="me-2"
-            prepend-icon="mdi-plus"
-            rounded="lg"
-            text="添加商品"
-            border
-            @click="add"
-          ></v-btn>
+          <v-dialog
+            v-model="dialog"
+            max-width="500px"
+          >
+            <template #activator="{props}">
+              <v-btn
+                border
+                class="me-2"
+                prepend-icon="mdi-plus"
+                rounded="lg"
+                text="添加商品"
+                v-bind="props"
+              ></v-btn>
+            </template>
+            <v-card
+              prepend-icon="mdi-shopping"
+              title="添加商品"
+              subtitle="为产品添加对应的商品品牌"
+            >
+              <v-form>
+                <v-text-field
+                  v-model="addGoodsDialog.name"
+                  class="mx-8 mb-2"
+                  label="商品名称"
+                  :rules="[goodsNameRequired]"
+                />
+                <v-textarea
+                  v-model="addGoodsDialog.description"
+                  class="mx-8"
+                  label="描述"
+                />
+              </v-form>
+
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+
+                <v-btn
+                  text="关闭"
+                  variant="plain"
+                  @click="dialog = false"
+                ></v-btn>
+
+                <v-btn
+                  color="primary"
+                  text="添加"
+                  variant="tonal"
+                  @click="addGoodsButtonClicked"
+                ></v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+
+
         </v-toolbar>
       </template>
     </v-data-table>
